cd83c3047ad247ce93980afac45a2b0f99436137
Phongphun Kijsanayothin <kphongph@gmail.com>
2014-06-17 09:18:28 -0500

    init tds

---
+++ tds/tds_org.js
@@ -0,0 +1,170 @@
+var csv = require('fast-csv');
+var fs = require('fs');
+var _ = require('underscore');
+
+function readCSV(path,callback) {
+  var content = [];
+  csv.fromPath(path,{headers:true})
+  .on('record',function(data) {
+    content.push(data);
+  })
+  .on('end',function() {
+    callback(content);
+  });
+}
+
+function convert_int(table,taxonomy,col_name) {
+  var c_list = [];
+  var p_list = [];
+  for(var j=0;j<taxonomy.length;j++) {
+    if(taxonomy[j].tree == col_name) {
+      c_list.push(taxonomy[j].child);
+      p_list.push(taxonomy[j].parent);
+    }
+  }
+  
+  var child_list={};
+  var regex = /\[([0-9]+)-([0-9]+)\)/i;
+  for(var i=0;i<c_list.length;i++) {
+    if(p_list.indexOf(c_list[i])==-1) {
+      child_list[c_list[i]] = {};
+      var result=c_list[i].match(regex);
+      child_list[c_list[i]] = {'min':result[1],'max':result[2]};
+    }
+  }
+  
+  for(var i=0;i<table.length;i++) {
+    var int_value = parseInt(table[i][col_name]);
+    for(var key in child_list) {
+      if(int_value >= child_list[key].min && int_value < child_list[key].max) {
+        table[i][col_name]=key;
+      }
+    }
+  }
+ // console.log(table);
+}
+
+function compute_row(table) {
+  var dict={};
+  var row=0;
+  for(var i=0;i<table.length;i++) {
+    var row_key ='';
+    for(var key in table[i]) {
+      if(key!="Class") {
+        row_key+='('+table[i][key]+')';
+      }
+    }
+    if(!dict[row_key]) {
+      row++;
+      dict[row_key]=0;
+    }
+    dict[row_key]++;
+  }
+ // console.log(dict);
+  return row;
+}
+
+function check_constraint(table,list,freq) {
+  var dict={}; 
+  for(var i=0;i<table.length;i++) {
+    var key ='';
+    var value_dict = {};
+    for(var j=0;j<list.length;j++) {
+      key+='('+table[i][list[j]]+')';
+      value_dict[list[j]]=table[i][list[j]];
+    }
+    if(!dict[key]) {
+      dict[key]={'freq':0,'value':value_dict};
+    }
+    dict[key].freq++;
+  }
+  var pass=true;
+  var choice = [];
+  for(var key in dict) {
+    if(dict[key].freq<freq) {
+      choice.push(dict[key]);
+    }
+  }
+  return choice;
+}
+
+function modify_table(table,taxonomy,cutpoint) {
+  var n_table = JSON.parse(JSON.stringify(table));
+  for(var key in cutpoint) {
+    var map = {};
+    var tree = cutpoint[key];
+    for(var i=0;i<n_table.length;i++) {
+      if(!map[n_table[i][key]]) {
+        var end_search=false;
+        var found = false;
+        var cur_key = n_table[i][key];
+        while(!end_search && !found) {
+          if(cutpoint[key].indexOf(cur_key)==-1) {
+            end_search = true;
+            for(var j=0;j<taxonomy.length;j++) {
+              if(taxonomy[j].tree == key && taxonomy[j].child == cur_key) {
+                cur_key=taxonomy[j].parent;
+                end_search = false;
+              }
+            }
+          } else {
+            found=true;
+          }
+        }
+        if(found) {
+          map[n_table[i][key]]=cur_key;
+        } else {
+          map[n_table[i][key]]=n_table[i][key];
+        }
+      }
+      n_table[i][key]=map[n_table[i][key]];
+    }
+  }
+  return n_table;
+}
+
+function init(tab_csv,tax_csv,int_cols,cb) {
+  readCSV(tab_csv,function(raw_table) {
+    readCSV(tax_csv,function(taxonomy) {
+      for(var i=0;i<int_cols.length;i++) {
+        convert_int(raw_table,taxonomy,int_cols[i]);
+      }
+      cb(raw_table,taxonomy);
+    });
+  });
+}
+
+function isDuplicate(list,cutpoint) {
+  for(var i=0;i<list.length;i++) {
+    if(_.isEqual(list[i].cutpoint,cutpoint)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function generate_node(node_list,cp_list,taxonomy,raw_table,cons) {
+  var c_node = node_list.shift();
+  var n_table = modify_table(raw_table,taxonomy,c_node.cutpoint);
+}
+
+function algorithm_1(tab_csv,tax_csv,int_cols,cons) {
+  var list_nodes = [];
+  var cutpoint_list = [];
+  init(tab_csv,tax_csv,int_cols,function(raw_table,taxonomy) {
+    var cutpoint = {};
+    for(var key in raw_table[0]) {
+      cutpoint[key] = 'ANY';
+    }
+    list_nodes.push({'cutpoint':cutpoint});
+    generate_node(list_nodes,cutpoint_list,taxonomy,raw_table,cons);
+  });
+}
+
+algorithm_1('sample.csv','taxonomy.csv',['Work'],
+  [
+    {'cols':['Education','Sex'],'freq':4}, 
+    {'cols':['Sex','Work'],'freq':11}, 
+  ]
+);
+
